1da696d22d146c906faa929c35e28169f93951f4
HankHint <hankhint@gmail.com>
2021-11-18 15:12:21 -0600

    switched from bootstrap to materialize

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+var obj = {
+"hank stuff": "car",
+
+}
+//dot notation v bracket notation
+// bracket notation: can use a variable as the key
+//dot notation: can't have spaces in the key name, nor can you use a variable as the key
+
+
+
+// localStorage.setItem("facts", JSON.stringify({firstNum: [1, 2, 3], planet: "neptune"}));
+
+// var hankStuff = JSON.parse(localStorage.getItem("facts"));
+
+// console.log(hankStuff["firstNum"]);
+// console.log(hankStuff.firstNum);
+
+// //key -- value
+// //string(first argument in localstorage.setitem) is the name of the key in localStorage
+// localStorage.setItem("alphabet", 123);
+
+
+//stringify
+// //parseInt parseFloat
+// console.log(parseInt(localStorage.getItem("alphabet")));
+// console.log(typeof 123);
+// //typeof is an operator
