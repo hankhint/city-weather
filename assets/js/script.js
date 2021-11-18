5c81035bc555625cae20170b58e85325d87d1bb7
HankHint <hankhint@gmail.com>
2021-11-18 17:33:32 -0600

    did a bunch of work with my tutor about text input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,70 @@
-
-
 var displayWeather = function (city, data) {
-    var iconCode = data.weather[0].icon;
-    var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    var cityName = document.getElementById("cityName");
-    var cityTemp = document.getElementById("cityTemp");
-    document.getElementById("weatherIcon").setAttribute("src", iconURL);
-    cityName.textContent = data.name;
-    cityTemp.textContent = Math.round(data.main.temp) + " °F";
-  };
+  console.log(data.weather[0]);
+  var iconCode = data.weather[0].icon;
+  var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+  // console.log(iconURL)
+  var cityName = document.getElementById("cityName");
+  var cityTemp = document.getElementById("cityTemp");
+  var userInputCityName = document.getElementById("userInputCityName");
+  document.getElementById("weatherIcon").setAttribute("src", iconURL);
+  cityName.textContent = data.name;
+  cityTemp.textContent = Math.round(data.main.temp) + " °F";
+};
+
+var searchButtonEl = document.getElementById("userCityNameSubmit");
 
 var callCity = function (city) {
-    var apiKey = "&appid=0cab3455fdc5081541be5d657005bb3b";
-    var callCityURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      apiKey +
-      "&units=imperial";
-    fetch(callCityURL)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            console.log(data);
-            displayWeather(city, data);
-          });
-        } else {
-          alert("Error:" + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert("Unable to connect");
-      });
-  };
-  
+  var apiKey = "&appid=0cab3455fdc5081541be5d657005bb3b";
+  var callCityURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    apiKey +
+    "&units=imperial";
+  fetch(callCityURL)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          // console.log(data);
+          displayWeather(city, data);
+        });
+      } else {
+        alert("Error:" + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect");
+    });
+};
+localStorage.setItem("cityLocalStorage",[] )
+var formSubmitHandler = function (event) {
+  // event.preventDefault();
 
-var getWeatherApi = function () {
-      callCity("Honolulu");
+  //taking input from html and putting it into javascript
+  var userInputCityName = document.getElementById("userInputCityName");
+  console.log(userInputCityName);
 
-    };
+  //get value from input element and takes away any spaces on the left or right side, but not in the middle
+  var userSearchCityName = userInputCityName.value.trim();
 
+  if (userSearchCityName) {
+    callCity(userSearchCityName);
+    userInputCityName.value = "";
+    // callCity(userSearchCityName);
+    //console.log(event);
+    // get local storage
+    var citiesPreviouslySearched = localStorage.getItem("cityLocalStorage");
+    // adding city name to list of previously searched names
+    citiesPreviouslySearched.push(userSearchCityName);
+    //adding City name to local storage
+    localStorage.setItem("cityLocalStorage", citiesPreviouslySearched);
+  } else {
+    alert("Please enter a city name");
+  }
+};
 
 //dot notation v bracket notation
 // bracket notation: can use a variable as the key
 //dot notation: can't have spaces in the key name, nor can you use a variable as the key
-
-
 
 // localStorage.setItem("facts", JSON.stringify({firstNum: [1, 2, 3], planet: "neptune"}));
 
@@ -57,13 +77,10 @@ var getWeatherApi = function () {
 // //string(first argument in localstorage.setitem) is the name of the key in localStorage
 // localStorage.setItem("alphabet", 123);
 
-
 //stringify
 // //parseInt parseFloat
 // console.log(parseInt(localStorage.getItem("alphabet")));
 // console.log(typeof 123);
 // //typeof is an operator
 
-
-
-getWeatherApi();
+searchButtonEl.addEventListener("click", formSubmitHandler);
