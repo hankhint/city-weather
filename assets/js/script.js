ad1a267f1ecff720ed1a23fb73b85a92184852ca
HankHint <hankhint@gmail.com>
2021-11-18 15:35:49 -0600

    took the weather API call from group project 1, PokeParent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,45 @@
 
-var obj = {
-"hank stuff": "car",
 
-}
+var displayWeather = function (city, data) {
+    var iconCode = data.weather[0].icon;
+    var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    var cityName = document.getElementById("cityName");
+    var cityTemp = document.getElementById("cityTemp");
+    document.getElementById("weatherIcon").setAttribute("src", iconURL);
+    cityName.textContent = data.name;
+    cityTemp.textContent = Math.round(data.main.temp) + " °F";
+  };
+
+var callCity = function (city) {
+    var apiKey = "&appid=0cab3455fdc5081541be5d657005bb3b";
+    var callCityURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      apiKey +
+      "&units=imperial";
+    fetch(callCityURL)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+            displayWeather(city, data);
+          });
+        } else {
+          alert("Error:" + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert("Unable to connect");
+      });
+  };
+  
+
+var getWeatherApi = function () {
+      callCity("Honolulu");
+
+    };
+
+
 //dot notation v bracket notation
 // bracket notation: can use a variable as the key
 //dot notation: can't have spaces in the key name, nor can you use a variable as the key
@@ -26,3 +63,7 @@ var obj = {
 // console.log(parseInt(localStorage.getItem("alphabet")));
 // console.log(typeof 123);
 // //typeof is an operator
+
+
+
+getWeatherApi();
